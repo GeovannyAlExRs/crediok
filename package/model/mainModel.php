8098920bc452e01f7722747af72c18c0350acd6f
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-01-11 23:25:18 -0500

    Function Encrypt, Decrypt and Generate code random

--- package/model/mainModel.php
+++ package/model/mainModel.php
@@ -21,4 +21,32 @@
             $sql -> execute();
             return $sql;
         }
+
+        /*** [FUNCTION ENCRYPT CADENA] ***/
+		public function encryption($string){
+			$output=FALSE;
+			$key=hash('sha256', SECRET_KEY);
+			$iv=substr(hash('sha256', SECRET_IV), 0, 16);
+			$output=openssl_encrypt($string, METHOD, $key, 0, $iv);
+			$output=base64_encode($output);
+			return $output;
+		}
+
+        /*** [FUNCTION DENCRYPT CADENA] ***/
+		protected static function decryption($string){
+			$key=hash('sha256', SECRET_KEY);
+			$iv=substr(hash('sha256', SECRET_IV), 0, 16);
+			$output=openssl_decrypt(base64_decode($string), METHOD, $key, 0, $iv);
+			return $output;
+		}
+
+        /*** [FUNCTION GENERATE CODE RANDOM] ***/
+        protected static function generateCodeRandom($letter, $longitude, $digit){
+            for($i = 1; $i <= $longitude; $i++) {
+                $random = rand(0,9);
+                $letter.= $random;
+            }
+            return $letter."-".$digit;
+		}
+
     }
